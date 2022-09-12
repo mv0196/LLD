9a942323818f3f229e30fbe63aebdb25ca681b29
Mukul Verma <mukulverma3148@gmail.com>
2022-09-12 21:25:52 +0530

    Created clean structure

--- ObserverDesignPattern/ObserverDesignPattern/Program.cs
+++ ObserverDesignPattern/ObserverDesignPattern/Program.cs
@@ -8,6 +8,7 @@ namespace ObserverDesignPattern
     {
         //https://docs.google.com/document/d/1O2MSNIC_egBLhLJsiRng2XfBvKpyk0Mp_fd-qk0e47A/edit
         //https://www.youtube.com/watch?v=Ep9_Zcgst3U&list=PL6W8uoQQ2c61X_9e6Net0WdYZidm7zooW&index=3
+        //https://refactoring.guru/design-patterns/observer
         static void Main(string[] args)
         {
             IStockObservable phoneObservable = new PhoneStockObservable();
--- StrategyDesignPattern/StrategyDesignPattern/Program.cs
+++ StrategyDesignPattern/StrategyDesignPattern/Program.cs
@@ -6,6 +6,7 @@ namespace StrategyDesignPattern
     {
         //https://docs.google.com/document/d/1O2MSNIC_egBLhLJsiRng2XfBvKpyk0Mp_fd-qk0e47A/edit
         //https://www.youtube.com/watch?v=u8DttUrXtEw&list=PL6W8uoQQ2c61X_9e6Net0WdYZidm7zooW&index=2&t=619s
+        //https://refactoring.guru/design-patterns/strategy
         static void Main(string[] args)
         {
             Vehicle vh = new PassengerVehicle();
