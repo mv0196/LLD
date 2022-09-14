c371feef6ad787c1b1a99b66284d33bb3639d3d9
Mukul Verma <mukulverma3148@gmail.com>
2022-09-14 09:47:30 +0530

    Added docs in Factory Design Pattern

--- FactoryDesignPattern/FactoryDesignPattern/Program.cs
+++ FactoryDesignPattern/FactoryDesignPattern/Program.cs
@@ -6,6 +6,9 @@ namespace FactoryDesignPattern
 {
     public class Program
     {
+        //https://docs.google.com/document/d/1O2MSNIC_egBLhLJsiRng2XfBvKpyk0Mp_fd-qk0e47A/edit
+        //https://www.youtube.com/watch?v=7g9S371qzwM&list=PL6W8uoQQ2c61X_9e6Net0WdYZidm7zooW&index=5
+        //https://refactoring.guru/design-patterns/factory-method
         static void Main(string[] args)
         {
             ShapeFactory factory = new ShapeFactory();
